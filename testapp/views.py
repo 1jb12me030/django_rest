c4a861fbf73fcb6dbb1385d0a5991747b81ea802
mayank kumar <80468358+1jb12me030@users.noreply.github.com>
2022-06-08 10:13:11 +0530

    initial commit

---
+++ testapp/views.py
@@ -0,0 +1,11 @@
+from .models import Employee
+from .serializers import EmployeeSerializer
+from rest_framework import viewsets
+
+from rest_framework.authentication import TokenAuthentication
+
+from rest_framework.permissions import IsAuthenticated
+# Create your views here.
+class EmployeeModelViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class=EmployeeSerializer
